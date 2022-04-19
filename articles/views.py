213b2dfca737323c274b5a80bbc0e0d9d9494f23
Rakib Ali Tanzin <tanzin706@gmail.com>
2022-04-19 14:28:02 +0600

    lesson 26done

--- articles/views.py
+++ articles/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from contextlib import redirect_stderr
+from django.shortcuts import render,redirect
 from .models import Article
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from .import forms
 
 def article_list(request):
     #return HttpResponse('homepage')
@@ -14,5 +16,14 @@ def article_detail(request, slug):
     article = Article.objects.get(slug=slug)
     return render(request, 'articles/article_detail.html',{'art':article })
 @login_required(login_url='/accounts/login/')
+
+
 def article_create(request):
-    return render(request,'articles/article_create.html')
+    if request.method == 'POST':
+        form = forms.CreateArticle(request.POST,request.FILES)
+        if form.is_valid():
+            #save article to bd
+            return redirect('articles:list')
+    else:
+        form = forms.CreateArticle()
+    return render(request,'articles/article_create.html',{'form':form})
